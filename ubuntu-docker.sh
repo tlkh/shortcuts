20ad5beb322e7b5e2c431fa0605a401ebe748554
Timothy Liu <tlkh@live.co.uk>
2019-01-09 13:51:25 +0800

    Update ubuntu-docker.sh

--- ubuntu-docker.sh
+++ ubuntu-docker.sh
@@ -22,10 +22,7 @@ apt-get install -y \
     software-properties-common
 
 curl -fsSL https://download.docker.com/linux/ubuntu/gpg | apt-key add -
-
-apt-key fingerprint 0EBFCD88
-
-sudo add-apt-repository \
+add-apt-repository \
    "deb [arch=amd64] https://download.docker.com/linux/ubuntu \
    $(lsb_release -cs) \
    stable"
