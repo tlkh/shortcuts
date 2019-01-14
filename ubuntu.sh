3966c2608fe1e11172a7e3d4edc530ca82d7233a
Timothy Liu <tlkh@live.co.uk>
2019-01-14 10:16:25 +0800

    ask echo to accept backslash

--- ubuntu.sh
+++ ubuntu.sh
@@ -1,14 +1,14 @@
 #!/bin/sh
 
-echo "Installation script for Docker CE and nvidia-docker on Ubuntu 16+"
+echo -e "Installation script for Docker CE and nvidia-docker on Ubuntu 16+"
 
-echo "Set non-interactive frontend"
-echo "Script will run without any prompts"
+echo -e "Set non-interactive frontend"
+echo -e "Script will run without any prompts"
 export DEBIAN_FRONTEND=noninteractive
 
-echo "\n###\n"
-echo "Installing prerequisites for Docker CE"
-echo "\n###\n"
+echo -e "\n###\n"
+echo -e "Installing prerequisites for Docker CE"
+echo -e "\n###\n"
 
 apt-get update
 apt-get remove docker docker-engine docker.io -y
@@ -29,19 +29,19 @@ sudo add-apt-repository \
    $(lsb_release -cs) \
    stable"
 
-echo "\n###\n"
-echo "Installing Docker CE"
-echo "Version: 18.06.1 (Kubernetes-compatible)"
-echo "\n###\n"
+echo -e "\n###\n"
+echo -e "Installing Docker CE"
+echo -e "Version: 18.06.1 (Kubernetes-compatible)"
+echo -e "\n###\n"
 
 apt-get update
 apt-get install docker-ce=18.06.1~ce~3-0~ubuntu -y
 
-echo "\n###\n"
-echo "Installing NVIDIA drivers and CUDA"
-echo "Driver version: 410.79"
-echo "CUDA version: 10.0.130-1"
-echo "\n###\n"
+echo -e "\n###\n"
+echo -e "Installing NVIDIA drivers and CUDA"
+echo -e "Driver version: 410.79"
+echo -e "CUDA version: 10.0.130-1"
+echo -e "\n###\n"
 
 wget https://developer.download.nvidia.com/compute/cuda/repos/ubuntu1804/x86_64/cuda-repo-ubuntu1804_10.0.130-1_amd64.deb
 
@@ -51,27 +51,27 @@ apt-key adv --fetch-keys https://developer.download.nvidia.com/compute/cuda/repo
 apt-get update
 apt-get install cuda -y
 
-echo "\n###\n"
-echo "Installing nvidia-docker"
-echo "Version: Version: 18.06.1 (Kubernetes-compatible)"
-echo "\n###\n"
+echo -e "\n###\n"
+echo -e "Installing nvidia-docker"
+echo -e "Version: Version: 18.06.1 (Kubernetes-compatible)"
+echo -e "\n###\n"
 
 curl -s -L https://nvidia.github.io/nvidia-docker/gpgkey | apt-key add -
 
-distribution=$(. /etc/os-release;echo $ID$VERSION_ID)
+distribution=$(. /etc/os-release;echo -e $ID$VERSION_ID)
 
 curl -s -L https://nvidia.github.io/nvidia-docker/$distribution/nvidia-docker.list | tee /etc/apt/sources.list.d/nvidia-docker.list
 
 apt-get update
 apt-get install nvidia-docker2=2.0.3+docker18.06.1-1 nvidia-container-runtime=2.0.0+docker18.06.1-1 -y
 
-echo "\nInstalled nvidia-docker\n"
+echo -e "\nInstalled nvidia-docker\n"
 
-echo "\n###\n"
-echo "Finished with no errors."
-echo "\n\n[TIP]\nDon't want to run Docker with sudo?"
-echo "Add your own user by running 'usermod -aG docker $USER' normally"
-echo "\n\nSystem will now reboot"
-echo "\n###\n"
+echo -e "\n###\n"
+echo -e "Finished with no errors."
+echo -e "\n\n[TIP]\nDon't want to run Docker with sudo?"
+echo -e "Add your own user by running 'usermod -aG docker $USER' normally"
+echo -e "\n\nSystem will now reboot"
+echo -e "\n###\n"
 
 reboot
